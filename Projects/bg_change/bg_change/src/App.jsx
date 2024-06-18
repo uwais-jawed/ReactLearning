e627ebc4d191759db71af8d2928d25f821d96256
Uwais Jawed <142025750+uwais-jawed@users.noreply.github.com>
2024-06-18 23:45:05 +0530

    bg changer completed

--- Projects/bg_change/bg_change/src/App.jsx
+++ Projects/bg_change/bg_change/src/App.jsx
@@ -4,17 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  [current, changColor] = useState('black')
-
-  function changeColor(){
-    const body = document.querySelector(body)
-    body.setAttribute('backgroundColor','current')
-  }
+  const [color , setColor] = useState("")
 
   return (
-    <>
-    <button onClick={changeColor}>'Color change'</button>
-    </>
+    <div className="h-screen w-full" style={{backgroundColor : color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'>
+          <button
+          onClick={()=> setColor("red")}
+          className='outline-none px-4 py-4 rounded-md shadow-sm text-zinc-50' style={{backgroundColor :"red"}}>RED</button>
+
+          <button 
+          onClick={()=> setColor("green")}
+          className='outline-none px-4 py-4 rounded-md shadow-sm text-zinc-50' style={{backgroundColor :"green"}}>GREEN</button>
+
+          <button 
+          onClick={()=> setColor("blue")}
+          className='outline-none px-4 py-4 rounded-md shadow-sm text-zinc-50' style={{backgroundColor :"blue"}}>BLUE</button>
+
+          <button 
+          onClick={()=> setColor("black")}
+          className='outline-none px-4 py-4 rounded-md shadow-sm text-zinc-50 ' style={{backgroundColor :"black"}}>BLACK</button>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
